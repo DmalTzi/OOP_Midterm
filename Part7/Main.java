ebd24f0c44e5b0b2dd3428d345f673bd63ac8fd5
Tests <Audyomg@gmail.com>
2025-01-25 01:01:00 +0700

    .

--- Part7/Main.java
+++ Part7/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car("IndyZozo", 1880);
         car.showInfo();
         car.start();
         car.engine.refill(10);

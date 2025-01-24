9d28c6d713abe91afacea6f36ed26ad141517536
Tests <Audyomg@gmail.com>
2025-01-25 01:25:44 +0700

    Part8

---
+++ Part8/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Write Your lambda
+        System.out.println(cir.area(2));
+
+        // Write Your lambda
+        System.out.println(rec.area(5, 2));
+
+        // Write Your lambda
+        System.out.println(squ.area(2));
+
+        // Write Your lambda
+        System.out.println(tri.area(2,2));
+    }
+}
